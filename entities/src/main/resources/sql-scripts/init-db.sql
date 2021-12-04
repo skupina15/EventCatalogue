04916b288d1ffb57ff2a701ef5bcce8767f596e3
janastremfelj <jana.stremfelj98@gmail.com>
2021-12-04 15:38:51 +0100

    merged

--- entities/src/main/resources/sql-scripts/init-db.sql
+++ entities/src/main/resources/sql-scripts/init-db.sql
@@ -1,4 +1,4 @@
 INSERT INTO persone (id_persone, user_name, email) VALUES (1, 'user1', 'krneki@krneki.com');
 INSERT INTO item (id_item, title, description, daily_price, id_persone, tag) VALUES (11, 'neki', 'neki', 5, 1, 'neki');
---INSERT INTO persone (id_persone, user_name, email) VALUES (2, 'user2', 'krneki2@krneki.com');
+-- INSERT INTO persone (id_persone, user_name, email) VALUES (2, 'user2', 'krneki2@krneki.com');
 INSERT INTO event (id_event, title, description, start_date, end_date, id_persone, tag) VALUES (21, 'title', 'description', '2021-05-05', '2021-06-06', 1, 'tag');
