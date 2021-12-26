b609b4fc0f9f30844ed23e9444474c5a412934ba
janastremfelj <jana.stremfelj98@gmail.com>
2021-12-26 13:44:48 +0100

    discovery

--- api/src/main/java/si/fri/rso/skupina15/v1/EventCatalogueApplication.java
+++ api/src/main/java/si/fri/rso/skupina15/v1/EventCatalogueApplication.java
@@ -1,8 +1,12 @@
 package si.fri.rso.skupina15.v1;
 
+
+import com.kumuluz.ee.discovery.annotations.RegisterService;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+@RegisterService
 @ApplicationPath("v1")
 public class EventCatalogueApplication extends Application {
 }
--- api/src/main/java/si/fri/rso/skupina15/v1/resources/EventResource.java
+++ api/src/main/java/si/fri/rso/skupina15/v1/resources/EventResource.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.logging.Logger;
 
 @ApplicationScoped
-@Path("event")
+@Path("events")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class EventResource {
