222b0a35aaa85e35b47e3a3272cfcb8f395ef0b5
janastremfelj <jana.stremfelj98@gmail.com>
2021-12-29 20:08:42 +0100

    za frontend

--- api/src/main/java/si/fri/rso/skupina15/v1/EventCatalogueApplication.java
+++ api/src/main/java/si/fri/rso/skupina15/v1/EventCatalogueApplication.java
@@ -2,6 +2,7 @@ package si.fri.rso.skupina15.v1;
 
 
 //import com.kumuluz.ee.discovery.annotations.RegisterService;
+import com.kumuluz.ee.cors.annotations.CrossOrigin;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
@@ -15,5 +16,6 @@ import javax.ws.rs.core.Application;
 @OpenAPIDefinition(info = @Info(title = "API for events", version = "v1", contact = @Contact(email = "rso@fri.uni-lj.si"), license = @
         License(name = "dev"), description = "API for events."), servers = @Server(url ="http://20.120.67.65:8080/events/v1"))
 @ApplicationPath("v1")
+@CrossOrigin(name = "my-resource")
 public class EventCatalogueApplication extends Application {
 }
