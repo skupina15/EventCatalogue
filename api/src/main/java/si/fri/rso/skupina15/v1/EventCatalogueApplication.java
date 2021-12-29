0819eef6dfd369d5778d6be758388a9ae51d5baf
Gal petkovšek <gp1914@student.uni-lj.si>
2021-12-29 14:59:22 +0100

    openApi

--- api/src/main/java/si/fri/rso/skupina15/v1/EventCatalogueApplication.java
+++ api/src/main/java/si/fri/rso/skupina15/v1/EventCatalogueApplication.java
@@ -1,12 +1,19 @@
 package si.fri.rso.skupina15.v1;
 
 
-import com.kumuluz.ee.discovery.annotations.RegisterService;
+//import com.kumuluz.ee.discovery.annotations.RegisterService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@RegisterService
+//@RegisterService
+@OpenAPIDefinition(info = @Info(title = "API for events", version = "v1", contact = @Contact(email = "rso@fri.uni-lj.si"), license = @
+        License(name = "dev"), description = "API for events."), servers = @Server(url ="http://20.120.67.65:8080/events/v1"))
 @ApplicationPath("v1")
 public class EventCatalogueApplication extends Application {
 }
