b15a3b2a904e7b3532693f34533123c0b2d2f38a
Gal petkovšek <gp1914@student.uni-lj.si>
2022-01-07 10:44:14 +0100

    logging

--- api/src/main/java/si/fri/rso/skupina15/v1/resources/EventResource.java
+++ api/src/main/java/si/fri/rso/skupina15/v1/resources/EventResource.java
@@ -1,5 +1,6 @@
 package si.fri.rso.skupina15.v1.resources;
 
+import com.kumuluz.ee.logs.cdi.Log;
 import com.kumuluz.ee.rest.beans.QueryParameters;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -23,6 +24,7 @@ import java.util.List;
 import java.util.ServiceLoader;
 import java.util.logging.Logger;
 
+@Log
 @ApplicationScoped
 @Path("events")
 @Produces(MediaType.APPLICATION_JSON)
